866f268bcd8124b67f2bd2c17259ae0d160fa319
molinajulian <molina.bohemio@gmail.com>
2019-11-08 00:27:07 -0300

    Get people by id

--- app/controllers/people.js
+++ app/controllers/people.js
@@ -1,3 +1,7 @@
-const { getPeople } = require('../interactors/people');
+const { getPeople, getSpecificPerson } = require('../interactors/people');
+const { mapPersonData } = require('../serializers/swapi');
 
 exports.getAllPersons = (req, res, next) => getPeople().then(response => res.status(200).send(response));
+
+exports.getPerson = ({ params: { id } }, res, next) =>
+  getSpecificPerson({ id }).then(personData => res.status(200).send(mapPersonData(personData)));
